f09996f3fc0b6058b7ccc6c985e6e3bd2d547bd9
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-01-06 21:47:06 +0800

    add fonts and colors

---
+++ src/styles/colors.js
@@ -0,0 +1,5 @@
+export const
+    black2f= "#2f2f2f",
+    white= "#ffffff",
+    electric_green = "#2bd927",
+    very_light_pink = "#bababa"
---
+++ src/styles/fonts.js
@@ -0,0 +1,15 @@
+export const 
+    aganeS = {fontFamily: "Agane S 65"},
+    roboto = {fontFamily: "Roboto Regular"},
+    robotoItalic = {fontFamily: "Roboto Italic"},
+    robotoThin = {fontFamily: "Roboto Thin"},
+    robotoThinItalic = {fontFamily: "Roboto Thin Italic"},
+    robotoLight = {fontFamily: "Roboto Light"},
+    robotoLightItalic = {fontFamily: "Roboto Light Italic"},
+    robotoMedium = {fontFamily: "Roboto Medium"},
+    robotoMediumItalic = {fontFamily: "Roboto Medium Italic"},
+    robotoBold = {fontFamily: "Roboto Bold"},
+    robotoBoldItalic = {fontFamily: "Roboto Bold Italic"},
+    robotoBlack = {fontFamily: "Roboto Black"},
+    robotoBlackItalic = {fontFamily: "Roboto Black Italic"}
+    
