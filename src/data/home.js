68c47f2463661c5136cc7c5d332405f5d500e4f3
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-01-09 00:12:36 +0800

    home screen data

---
+++ src/data/home.js
@@ -0,0 +1,192 @@
+export const insight = [
+    {
+        id: "abcde",
+        images: [
+            {
+                url: "https://i.imgur.com/YxCwuqw.jpg"
+            }
+        ]
+    },
+    {
+        id: "defgh",
+        images: [
+            {
+                url: "https://i.imgur.com/YxCwuqw.jpg"
+            }
+        ]
+    },
+    {
+        id: "asdw23",
+        images: [
+            {
+                url: "https://i.imgur.com/YxCwuqw.jpg"
+            }
+        ]
+    }
+]
+
+export const topSessionPlayers = [
+    {
+        id: "05CRzFTp7TouOXPuH6Tapu",
+        name: "Isyana Sarasvati",
+        images: [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/66b8bf4cb3d7ab114c64587615ef674805f8ef92",
+                "width": 640
+            },
+            {
+                "height": 320,
+                "url": "https://i.scdn.co/image/b6008a15b43d04203b7a02806a5ea902d6285fe7",
+                "width": 320
+            },
+            {
+                "height": 160,
+                "url": "https://i.scdn.co/image/bdfe0626da17b315669691e09043fcea444ebf44",
+                "width": 160
+            }
+        ],
+        rating: 4.0,
+    },
+    {
+        id: "5OZXWMwDhlYBRvoOfcX0sk",
+        name: "Raisa",
+        images: [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/9625444a0fb302b8d3ee931858f6192696b850cf",
+                "width": 640
+            },
+            {
+                "height": 320,
+                "url": "https://i.scdn.co/image/5bf657240cf2005a145bf272c791476fd82a40b3",
+                "width": 320
+            },
+            {
+                "height": 160,
+                "url": "https://i.scdn.co/image/bbaf0f8e905055aed5523579d47b3b4c893a5156",
+                "width": 160
+            }
+        ],
+        rating: 4.0
+    },
+    {
+        id: "2iDVt6mFbtbDEZG5ax0dTi",
+        name: "Tulus",
+        images: [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/3265c969600254fb33c8dfb7c6f31b658f492f25",
+                "width": 640
+            },
+            {
+                "height": 320,
+                "url": "https://i.scdn.co/image/1c56c04ef035e91d71192a4ad78c8a424d458f48",
+                "width": 320
+            },
+            {
+                "height": 160,
+                "url": "https://i.scdn.co/image/98cf6ab5f13b40cea2e6de875b3ee45efcf604aa",
+                "width": 160
+            }
+        ],
+        rating: 4.0
+    },
+    {
+        id: "5iHfwaDNZ11Y0yAyQVYDmy",
+        name: "Virzha",
+        images: [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/070612671bd66617367bedeb24559cdb06ad629d",
+                "width": 640
+            },
+            {
+                "height": 300,
+                "url": "https://i.scdn.co/image/3b4a225f0920b3aba457c3b33d12bf869801528c",
+                "width": 300
+            },
+            {
+                "height": 64,
+                "url": "https://i.scdn.co/image/1b44505bdee381b80ccecf99d2c3f54016e15799",
+                "width": 64
+            }
+        ],
+        rating: 4.0
+    },
+    {
+        id: "28DdkLhrzQNizZ0ExQpyku",
+        name: "Rizky Febian",
+        images: [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/0bb46f3603da6b13d29f8b5f2272b6b688c39aed",
+                "width": 640
+            },
+            {
+                "height": 320,
+                "url": "https://i.scdn.co/image/900e178226bebc613b24d813c9b92f7d545564cc",
+                "width": 320
+            },
+            {
+                "height": 160,
+                "url": "https://i.scdn.co/image/a585ac9f311b4c11a4300e92a32ea8e7f3736b2b",
+                "width": 160
+            }
+        ],
+        rating: 4.0
+    }
+]
+
+export const upcomingEvents = [
+    {
+        id: "d1v",
+        name: "Authentic Moment With Fiersa Besari",
+        date: "2018-01-19",
+        images: [
+            {
+                url: "https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/banner/20181227062500_5c240dccdebb5.jpg"
+            }
+        ]
+    },
+    {
+        id: "erk",
+        name: "Authentic Moment With Efek Rumah Kaca",
+        date: "2018-01-18",
+        images: [
+            {
+                url: "https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/banner/20190102125734_5c2c52cf0058b.jpg"
+            }
+        ]
+    },
+    {
+        id: "44U9",
+        name: "Banten Music Festival",
+        date: "2018-01-18",
+        images: [
+            {
+                url: "https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/banner/20181003071811.jpg"
+            }
+        ]
+    },
+    {
+        id: "D1n",
+        name: "Tulus Monokrom",
+        date: "2018-02-06",
+        images: [
+            {
+                url: "https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/banner/20181228022140.jpg"
+            }
+        ]
+    },
+    {
+        id: "bYN",
+        name: "Love is Live",
+        date: "2018-02-08",
+        images: [
+            {
+                url: "https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/banner/20181116114647.JPG"
+            }
+        ]
+    }
+]
