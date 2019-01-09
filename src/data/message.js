d47a881f24a2bb4be1802df14c978be06d5e726d
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-01-09 14:13:13 +0800

    add message data

---
+++ src/data/message.js
@@ -0,0 +1,38 @@
+export const messages = [
+    {
+        id: "abcde",
+        name: "Isyana Sarasvati",
+        message: "Lorem ipsum dolor sit amet",
+        images: [
+            {
+                "height": 160,
+                "url": "https://i.scdn.co/image/bdfe0626da17b315669691e09043fcea444ebf44",
+                "width": 160
+            }
+        ]
+    },
+    {
+        id: "qwesd",
+        name: "Raisa",
+        message: "Lorem ipsum dolor sit amet",
+        images: [
+            {
+                "height": 160,
+                "url": "https://i.scdn.co/image/bbaf0f8e905055aed5523579d47b3b4c893a5156",
+                "width": 160
+            }
+        ]
+    },
+    {
+        id: "asdes",
+        name: "Virzha",
+        message: "Lorem ipsum dolor sit amet",
+        images: [
+            {
+                "height": 64,
+                "url": "https://i.scdn.co/image/1b44505bdee381b80ccecf99d2c3f54016e15799",
+                "width": 64
+            }
+        ]
+    },
+]
