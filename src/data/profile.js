3b928ef51ab92d081ba7d261b8be1554bb66cb10
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-01-10 06:45:34 +0800

    create players data

---
+++ src/data/profile.js
@@ -0,0 +1,372 @@
+export const players = [
+    {
+        id: "05CRzFTp7TouOXPuH6Tapu",
+        name: "Isyana Sarasvati",
+        location: "Bandung, Indonesia",
+        followers: 643,
+        fee: "> Rp. 300k",
+        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam gravida porttitor ante a accumsan. In hac habitasse platea dictumst. Donec aliquet accumsan felis sed rhoncus. Proin magna lacus, blandit in consequat quis, dapibus sit amet urna. Etiam pharetra nulla erat, quis mollis lectus pellentesque nec. Donec scelerisque gravida turpis.",
+        lastest_events: [
+
+        ],
+        skills: [
+            "Vocal", "Guitar", "Piano", "Jazz", "Pop"
+        ],
+        reviews: [
+            {
+                id: "abasasd",
+                user: {
+                    id: "5OZXWMwDhlYBRvoOfcX0sk",
+                    name: "Raisa",
+                    images: [
+                        {
+                            "height": 640,
+                            "url": "https://i.scdn.co/image/9625444a0fb302b8d3ee931858f6192696b850cf",
+                            "width": 640
+                        },
+                        {
+                            "height": 320,
+                            "url": "https://i.scdn.co/image/5bf657240cf2005a145bf272c791476fd82a40b3",
+                            "width": 320
+                        },
+                        {
+                            "height": 160,
+                            "url": "https://i.scdn.co/image/bbaf0f8e905055aed5523579d47b3b4c893a5156",
+                            "width": 160
+                        }
+                    ],
+                },
+                event: {
+                    id: "asdasdcs",
+                    name: "Java Jazz"
+                },
+                review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus lacus libero, a elementum ligula.",
+                rating: 3.5,
+                date: "2018-01-07"
+            }
+        ],
+        works: [
+            {
+                id: "4OTR3hEos8V9FqPZ754knV",
+                images: [
+                    {
+                      "height": 640,
+                      "url": "https://i.scdn.co/image/17a6928409666c747a64ff4b2af091c41d5c14f3",
+                      "width": 640
+                    },
+                    {
+                      "height": 300,
+                      "url": "https://i.scdn.co/image/e6b3f454be2e49d6f9196dbb310c7172162ce4fb",
+                      "width": 300
+                    },
+                    {
+                      "height": 64,
+                      "url": "https://i.scdn.co/image/5e2c9c7f816fc12f8bfe219f00db9b950a8ab69f",
+                      "width": 64
+                    }
+                  ],
+                  name: "Paradox",
+            },
+            {
+                "id": "6TXaOwBLoyJrD6vem7L8Ba",
+                "images": [
+                    {
+                    "height": 640,
+                    "url": "https://i.scdn.co/image/d57027690203f7804011f19725c8fcf0ced20259",
+                    "width": 640
+                    },
+                    {
+                    "height": 300,
+                    "url": "https://i.scdn.co/image/eaeca7acefa489e16bdf9cc7a25c3138fee4b268",
+                    "width": 300
+                    },
+                    {
+                    "height": 64,
+                    "url": "https://i.scdn.co/image/7fac1ddb941e597ac963b0b39c1ab47fedaef69d",
+                    "width": 64
+                    }
+                ],
+                "name": "EXPLORE!",
+            },
+            {
+                "id": "6e1VW7tbrT9CRkxF8KtVbW",
+                "images": [
+                    {
+                    "height": 640,
+                    "url": "https://i.scdn.co/image/cdd44eb2c3eba7b23373c52cd5818d1805d5e03e",
+                    "width": 640
+                    },
+                    {
+                    "height": 300,
+                    "url": "https://i.scdn.co/image/d9a9b00ecc3427e56b35ea4baf7dbb8f08004f5b",
+                    "width": 300
+                    },
+                    {
+                    "height": 64,
+                    "url": "https://i.scdn.co/image/888e080aa47eb451291e78c528e0fd87e16abe92",
+                    "width": 64
+                    }
+                ],
+                "name": "Asia's Who We Are",
+            },
+            {
+                "id": "3GJjPtV0iPKM3fLPvqdURX",
+                "images": [
+                    {
+                    "height": 640,
+                    "url": "https://i.scdn.co/image/f24bfa17d79995435d0ba5ca7648189f92b4934c",
+                    "width": 640
+                    },
+                    {
+                    "height": 300,
+                    "url": "https://i.scdn.co/image/1b301aad112c917c1177d13e985268a03e1e3574",
+                    "width": 300
+                    },
+                    {
+                    "height": 64,
+                    "url": "https://i.scdn.co/image/3719423865806da308a897bd13215552a8de6c7d",
+                    "width": 64
+                    }
+                ],
+                "name": "Heaven",
+            }
+        ],
+        images: [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/66b8bf4cb3d7ab114c64587615ef674805f8ef92",
+                "width": 640
+            },
+            {
+                "height": 320,
+                "url": "https://i.scdn.co/image/b6008a15b43d04203b7a02806a5ea902d6285fe7",
+                "width": 320
+            },
+            {
+                "height": 160,
+                "url": "https://i.scdn.co/image/bdfe0626da17b315669691e09043fcea444ebf44",
+                "width": 160
+            }
+        ],
+        rating: 4.0,
+    },
+    {
+        id: "5OZXWMwDhlYBRvoOfcX0sk",
+        name: "Raisa",
+        works: [
+            {
+                "id": "4qjxXBBkd1WGW1lz6ibLMy",
+                "images": [
+                    {
+                    "height": 640,
+                    "url": "https://i.scdn.co/image/a6f00cc2e8da7aa7a97d13b9121d3f372657a4a2",
+                    "width": 640
+                    },
+                    {
+                    "height": 300,
+                    "url": "https://i.scdn.co/image/301adbf84ad5edf5a72038943c2ba1a6e133bda4",
+                    "width": 300
+                    },
+                    {
+                    "height": 64,
+                    "url": "https://i.scdn.co/image/74ac0c90617b3298a8206c916005ff6e7af2005b",
+                    "width": 64
+                    }
+                ],
+                "name": "Handmade",
+            },
+            {
+                "id": "4BZl8mzxQXW0CeOWEV4vek",
+                "images": [
+                    {
+                    "height": 640,
+                    "url": "https://i.scdn.co/image/2a416c0c594c00cb82a373d92bc08bad9aad56b1",
+                    "width": 640
+                    },
+                    {
+                    "height": 300,
+                    "url": "https://i.scdn.co/image/d897807a31da7a5f787b7dbc8cf0f0601d1f1fd1",
+                    "width": 300
+                    },
+                    {
+                    "height": 64,
+                    "url": "https://i.scdn.co/image/87b9ebdddf3b8607b0c49e7975e349353dbe6079",
+                    "width": 64
+                    }
+                ],
+                "name": "Raisa",
+            },
+            {
+                "id": "4OaVx6l378wjkKkWEfSuj9",
+                "images": [
+                {
+                    "height": 640,
+                    "url": "https://i.scdn.co/image/b726bf3ec7dcaf55e1b1dc6d6691a699099dc747",
+                    "width": 640
+                },
+                {
+                    "height": 300,
+                    "url": "https://i.scdn.co/image/733a3342874284be5fbd65ca36ede201718749a6",
+                    "width": 300
+                },
+                {
+                    "height": 64,
+                    "url": "https://i.scdn.co/image/9e7775b4a3eb6b3d00b9520f84e966f87dc1ef63",
+                    "width": 64
+                }
+                ],
+                "name": "Heart to Heart",
+            },
+            {
+                "id": "6SERoKcQlhROnYfKPjDAaH",
+                "images": [
+                    {
+                    "height": 640,
+                    "url": "https://i.scdn.co/image/9b881aaacc84be6f48a15e74f3de0c1564d8fcb5",
+                    "width": 640
+                    },
+                    {
+                    "height": 300,
+                    "url": "https://i.scdn.co/image/4431acfd3fd40517d8549f76a3aee7adc5bb7fe1",
+                    "width": 300
+                    },
+                    {
+                    "height": 64,
+                    "url": "https://i.scdn.co/image/41184762feb4c22b94968fc3c539894952670197",
+                    "width": 64
+                    }
+                ],
+                "name": "Mine (Day)",
+            },
+            {
+                "id": "5QxSzOO4ak5PkgAd3tH3Kq",
+                "images": [
+                    {
+                    "height": 640,
+                    "url": "https://i.scdn.co/image/7687e6e35c7aa28b9f6332865fa04e15488da028",
+                    "width": 640
+                    },
+                    {
+                    "height": 300,
+                    "url": "https://i.scdn.co/image/f1589bdbdc13610fb6af6c9bc0a14f835dc44ce4",
+                    "width": 300
+                    },
+                    {
+                    "height": 64,
+                    "url": "https://i.scdn.co/image/cb9aca73b6ddeb34e9ad7e197a79aa53dc325601",
+                    "width": 64
+                    }
+                ],
+                "name": "Mine (Night)",
+            },
+            {
+                "id": "3e9hiNDFkeIx10Pt87h5Gk",
+                "images": [
+                    {
+                    "height": 640,
+                    "url": "https://i.scdn.co/image/cb0d2a760465a944a034b985d27389e760ac7e64",
+                    "width": 640
+                    },
+                    {
+                    "height": 300,
+                    "url": "https://i.scdn.co/image/2649e1cd4453b01f495c3f69194a25abe21bd75f",
+                    "width": 300
+                    },
+                    {
+                    "height": 64,
+                    "url": "https://i.scdn.co/image/d86687fd5c63ac4c2f471e6840a143df8282fbf7",
+                    "width": 64
+                    }
+                ],
+                "name": "My Kind of Crazy",
+            },
+        ],
+        images: [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/9625444a0fb302b8d3ee931858f6192696b850cf",
+                "width": 640
+            },
+            {
+                "height": 320,
+                "url": "https://i.scdn.co/image/5bf657240cf2005a145bf272c791476fd82a40b3",
+                "width": 320
+            },
+            {
+                "height": 160,
+                "url": "https://i.scdn.co/image/bbaf0f8e905055aed5523579d47b3b4c893a5156",
+                "width": 160
+            }
+        ],
+        rating: 4.0
+    },
+    {
+        id: "2iDVt6mFbtbDEZG5ax0dTi",
+        name: "Tulus",
+        works: [],
+        images: [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/3265c969600254fb33c8dfb7c6f31b658f492f25",
+                "width": 640
+            },
+            {
+                "height": 320,
+                "url": "https://i.scdn.co/image/1c56c04ef035e91d71192a4ad78c8a424d458f48",
+                "width": 320
+            },
+            {
+                "height": 160,
+                "url": "https://i.scdn.co/image/98cf6ab5f13b40cea2e6de875b3ee45efcf604aa",
+                "width": 160
+            }
+        ],
+        rating: 4.0
+    },
+    {
+        id: "5iHfwaDNZ11Y0yAyQVYDmy",
+        name: "Virzha",
+        works: [],
+        images: [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/070612671bd66617367bedeb24559cdb06ad629d",
+                "width": 640
+            },
+            {
+                "height": 300,
+                "url": "https://i.scdn.co/image/3b4a225f0920b3aba457c3b33d12bf869801528c",
+                "width": 300
+            },
+            {
+                "height": 64,
+                "url": "https://i.scdn.co/image/1b44505bdee381b80ccecf99d2c3f54016e15799",
+                "width": 64
+            }
+        ],
+        rating: 4.0
+    },
+    {
+        id: "28DdkLhrzQNizZ0ExQpyku",
+        name: "Rizky Febian",
+        works: [],
+        images: [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/0bb46f3603da6b13d29f8b5f2272b6b688c39aed",
+                "width": 640
+            },
+            {
+                "height": 320,
+                "url": "https://i.scdn.co/image/900e178226bebc613b24d813c9b92f7d545564cc",
+                "width": 320
+            },
+            {
+                "height": 160,
+                "url": "https://i.scdn.co/image/a585ac9f311b4c11a4300e92a32ea8e7f3736b2b",
+                "width": 160
+            }
+        ],
+        rating: 4.0
+    }
+]
