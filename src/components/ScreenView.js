7e924e63308d7fafcf63dbf72ab5bb875ab340ed
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-01-11 05:26:53 +0800

    update screenview component

--- src/components/ScreenView.js
+++ src/components/ScreenView.js
@@ -5,7 +5,7 @@ import { white } from "../styles/colors";
 
 const Basic = ({backgroundImage, statusBarBackgroundColor, statusBarTranslucent = false, headerBar = false, headerBarStyle, headerTitle, headerTitleStyle, headerColor}) => (
     <React.Fragment>
-        <StatusBar barStyle="default" backgroundColor={statusBarBackgroundColor ? statusBarBackgroundColor : white} translucent={statusBarTranslucent} />
+        <StatusBar barStyle="dark-content" backgroundColor={statusBarBackgroundColor ? statusBarBackgroundColor : white} translucent={statusBarTranslucent} />
         { backgroundImage ? <Image source={backgroundImage} style={{width: '100%', height: Dimensions.get('window').width * 1.868613139, position: 'absolute', top: 0}} /> : null }
         { statusBarTranslucent ? <View style={{height: StatusBar.currentHeight}}></View> : null }
         { headerBar ? <HeaderBar title={headerTitle} titleStyle={headerTitleStyle} color={headerColor} semutyle={headerBarStyle} /> : null }
